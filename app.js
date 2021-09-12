0c9cc4f087c1a2e568474d4b8bb6b6f400be264c
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-12 14:26:39 -0400

    remove public, comment out 404 for repurposing

--- app.js
+++ app.js
@@ -13,15 +13,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// TODO: MODIFY FOR API catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
