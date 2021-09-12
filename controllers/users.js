24f2f35ab473665412f97032837bd14df22dcc0d
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-12 16:53:10 -0400

    fleshed out basic crud

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,46 @@
-exports.index = (req, res) => {
-  res.send("Hello World");
+const User = require("../models/User");
+
+exports.index = async (req, res) => {
+  try {
+    const users = await User.find().exec();
+    res.send(users);
+  } catch (error) {
+    res.send(error);
+  }
 };
-exports.show = (req, res) => {};
-exports.create = (req, res) => {};
-exports.update = (req, res) => {};
-exports.delete = (req, res) => {};
+
+exports.create = async (req, res) => {
+  try {
+    const user = await User.create({ name: req.body.user });
+    res.send(user);
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+exports.show = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.userId).exec();
+    res.send(user);
+  } catch (error) {
+    res.send(error);
+  }
+};
+
+exports.update = async (req, res) => {
+  try {
+    await User.updateOne({ _id: req.params.userId }, req.body);
+    res.send({ message: "success" });
+  } catch (error) {
+    res.status(500).send({ message: "failure" });
+  }
+};
+
+exports.delete = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.send({ message: "success" });
+  } catch (error) {
+    res.status(500).send({ error });
+  }
+};
