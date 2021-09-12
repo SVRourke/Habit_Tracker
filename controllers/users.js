351ced9ebf2db60a8a6e0f82d92b0e16e11c7357
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-12 15:43:48 -0400

    stubbed out user controller

---
+++ controllers/users.js
@@ -0,0 +1,7 @@
+exports.index = (req, res) => {
+  res.send("Hello World");
+};
+exports.show = (req, res) => {};
+exports.create = (req, res) => {};
+exports.update = (req, res) => {};
+exports.delete = (req, res) => {};
