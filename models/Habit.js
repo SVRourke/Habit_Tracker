da716f695014dfa2a3a9a6dd238e0f53f4d65b50
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-12 17:25:35 -0400

    started habit model

---
+++ models/Habit.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const habitSchema = new Schema({
+  habit: String,
+  all_day: Boolean,
+  goal_time: Date,
+  //   category: {type: Schema.Types.ObjectId, ref: "Category"}
+  //   logs: [{type: Schema.Types.ObjectId, ref: "Log"}]
+});
+
+const Habit = mongoose.model("Habit", habitSchema);
+module.exports = Habit;
+
+// const date = new Date(dateString);
+// date.setHours(hours, minutes);
