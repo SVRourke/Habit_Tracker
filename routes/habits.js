e38e77d4cef0bab08cc6368dfd61929db08dc318
Samuel Rourke <samuelvanrourke@gmail.com>
2021-09-12 17:46:57 -0400

    set up basic habits routes

--- routes/habits.js
+++ routes/habits.js
@@ -1,19 +1,17 @@
 var express = require("express");
-var router = express.Router();
+var router = express.Router({ mergeParams: true });
 
-// const habits = require("./habits");
 
 const controller = require("../controllers/habits");
 
 // router.get("/", (req, res) => res.send("Hello Habits"));
-// router.get("/", controller.index);
+router.get("/", controller.index);
 router.post("/", controller.create);
 
-// router
-//   .route("/:userId")
-//   .get(controller.show)
-//   .patch(controller.update)
-//   .delete(controller.delete);
-//   .use("/habits", habits)
+router
+  .route("/:habitId")
+  .get(controller.show)
+  .patch(controller.update)
+  .delete(controller.delete);
 
 module.exports = router;
