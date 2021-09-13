a2ed14943f3162d8434b6dada5f4d402525d0bd5
SVRourke <samuelvanrourke@gmail.com>
2021-09-13 16:04:47 -0400

    add references to user and habit

--- models/Log.js
+++ models/Log.js
@@ -4,6 +4,8 @@ const { Schema } = mongoose;
 const logSchema = new Schema(
   {
     success: Boolean, // default false
+    habit: { type: Schema.Types.ObjectId, ref: "Habit" },
+    user: { type: Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
 );
