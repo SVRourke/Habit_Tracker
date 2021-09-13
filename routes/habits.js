dd311f9c2c8227a864364a29f418b2f02f32f46e
SVRourke <samuelvanrourke@gmail.com>
2021-09-13 16:05:23 -0400

    adding logs router to habits router

--- routes/habits.js
+++ routes/habits.js
@@ -1,9 +1,8 @@
 var express = require("express");
 var router = express.Router({ mergeParams: true });
 
-
 const controller = require("../controllers/habits");
-
+const logsRouter = require("./logs");
 // router.get("/", (req, res) => res.send("Hello Habits"));
 router.get("/", controller.index);
 router.post("/", controller.create);
@@ -13,5 +12,7 @@ router
   .get(controller.show)
   .patch(controller.update)
   .delete(controller.delete);
+
+router.use("/:habitId/logs", logsRouter);
 
 module.exports = router;
