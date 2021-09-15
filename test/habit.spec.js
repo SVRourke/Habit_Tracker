e8501923f15861b76a36941d42270fa240d037d2
SVRourke <samuelvanrourke@gmail.com>
2021-09-15 17:43:18 -0400

    going back to basics , need to figure out why user is missing

--- test/habit.spec.js
+++ test/habit.spec.js
@@ -1,7 +1,11 @@
 const assert = require("assert");
 const { expect } = require("chai");
 
-const { dbConnect, dbDisconnect } = require("./test_helpers/dbConnect");
+const {
+  dbConnect,
+  dbDisconnect,
+  clearDB,
+} = require("./test_helpers/dbConnect");
 
 const User = require("../models/User");
 const Habit = require("../models/Habit");
@@ -10,48 +14,17 @@ const Log = require("../models/Log");
 describe("Habit Model", async function () {
   before(async function () {
     dbConnect(async () => {
-      // create User
       const user = await User.create({ name: "Sam" });
-      // create Habit
-      const habit = await Habit.create({
-        habit: "first habit",
-      });
-
-      //   add Habit to user
-      user.habits.push(habit);
-      await user.save();
-
-      // create Logs
-      const logInfos = [
-        { success: true, habit: habit, user: user, late: habit.isLate() },
-        { success: true, habit: habit, user: user, late: habit.isLate() },
-        { success: true, habit: habit, user: user, late: habit.isLate() },
-      ];
-      Log.insertMany(loginfos);
     });
-    // create testable records
   });
 
   it("can create & save a valid habit", async function () {
-    const habits = await Habit.find().exec();
-    
-    assert.equal(habits.length, 1);
+    const user = await User.find({ name: "Sam" }).exec();
+    assert.equal(1, 1);
   });
 
-  //   it("has logs", async function () {
-  //     const habit = await Habit.find().exec();
-  //     expect(habit[0].habit).to.be.eq("first habit");
-  //     assert.equal(habit[0].habit, "first habit");
-  //   });
-
-  //   VALIDATIONS
-  // "requires a birthday"
-
-  // runs once after the last test in this block
   after(async function () {
-    await User.deleteMany({});
-    await Habit.deleteMany({});
-    await Log.deleteMany({});
+    clearDB();
     dbDisconnect();
   });
 });
