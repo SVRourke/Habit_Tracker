b54f7124e1417cee1e626fde80b169c7295cb0ef
SVRourke <samuelvanrourke@gmail.com>
2021-09-16 08:07:35 -0400

    add method to create a habit

--- models/User.js
+++ models/User.js
@@ -1,12 +1,24 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const Habit = require("./Habit");
+
 const userSchema = new Schema({
   name: String,
   birthday: Date,
   habits: [{ type: Schema.Types.ObjectId, ref: "Habit" }],
   logs: [{ type: Schema.Types.ObjectId, ref: "Log" }],
 });
+
+userSchema.methods.createHabit = async function (habitAttributes) {
+  try {
+    const habit = new Habit({ ...habitAttributes, user: this.id });
+    await habit.save();
+    return habit;
+  } catch (error) {
+    return error;
+  }
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
