d3bea1ad3e93a22c04c6af9bc61f6bde894b1871
SVRourke <samuelvanrourke@gmail.com>
2021-09-16 08:07:48 -0400

    reworked

--- test/habit.spec.js
+++ test/habit.spec.js
@@ -13,14 +13,29 @@ const Log = require("../models/Log");
 
 describe("Habit Model", async function () {
   before(async function () {
-    dbConnect(async () => {
-      const user = await User.create({ name: "Sam" });
-    });
+    dbConnect();
   });
 
+  beforeEach(async function () {
+    const user = await User.create({ name: "Sam" });
+    // const habit = await Habit.create({ habit: "pushups", user: user._id });
+    const habit = await user.createHabit({
+      habit: "pushups",
+    });
+
+    await habit.createLog(true);
+    await habit.createLog(true);
+    await habit.createLog(true);
+  });
+  // afterEach(async function () {
+  //   clearDB();
+  // });
   it("can create & save a valid habit", async function () {
-    const user = await User.find({ name: "Sam" }).exec();
-    assert.equal(1, 1);
+    const user = await User.findOne({ name: "Sam" }).exec();
+    const habit = await Habit.findOne({ habit: "pushups" }).exec();
+    const logs = await Log.find({ habit: habit._id });
+
+    assert.equal(habit, habit);
   });
 
   after(async function () {
