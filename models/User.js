b512440f7b9178c17335cc8f2e09494fda993075
SVRourke <samuelvanrourke@gmail.com>
2021-09-17 10:50:28 -0400

    stub out drop habit method

--- models/User.js
+++ models/User.js
@@ -12,12 +12,27 @@ const userSchema = new Schema({
 
 userSchema.methods.createHabit = async function (habitAttributes) {
   try {
-    const habit = new Habit({ ...habitAttributes, user: this.id });
+    const habit = new Habit({ ...habitAttributes, user: this._id });
     await habit.save();
+
+    this.habits.push(habit);
+    await this.save();
     return habit;
   } catch (error) {
     return error;
   }
+};
+
+userSchema.methods.dropHabit = async function (habitId) {
+  try {
+    this.habits.pull({ _id: habitId });
+    await Habit.findOneAndDelete({ _id: habitId });
+  } catch (error) {
+    return error;
+  }
+  // find habit and delete
+
+  // remove habits from user
 };
 
 const User = mongoose.model("User", userSchema);
